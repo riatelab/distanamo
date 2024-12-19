4a9fa1deb7f4dbb7e65886e8b4221ff57320045a
rCarto <timothee.giraud@cnrs.fr>
2024-12-19 12:23:28 +0100

    Add pkg level documentation

---
+++ R/pkg.R
@@ -0,0 +1,11 @@
+#' @title Create Distance Cartograms
+#' @name distanamo
+#' @rdname distanamo
+#' @description
+#' This package enables the creation of what is often defined as a distance
+#' cartogram. This is done by showing (on background(s) layer(s), such as the
+#' territorial divisions of the study zone) the local deformations (calculated
+#' using Waldo Tobler's bidimensional regression) to fit image points to
+#' source points.
+#' @docType package
+"_PACKAGE"
