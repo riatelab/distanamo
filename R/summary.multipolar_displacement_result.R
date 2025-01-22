f4b5110b9458238ac4bfd30466e88502beded05b
Matthieu Viry <matthieu.viry@cnrs.fr>
2025-01-22 16:42:51 +0100

    Improve summary of adjustement step

--- R/summary.multipolar_displacement_result.R
+++ R/summary.multipolar_displacement_result.R
@@ -12,5 +12,11 @@ summary.multipolar_displacement_result <- function(object, ...) {
   cat("Min displacement:", summary_obj$min_displacement, paste0("[", units::deparse_unit(summary_obj$min_displacement), "]"), "\n")
   cat("Mean displacement:", summary_obj$mean_displacement, paste0("[", units::deparse_unit(summary_obj$mean_displacement), "]"), "\n")
   cat("Max displacement:", summary_obj$max_displacement, paste0("[", units::deparse_unit(summary_obj$max_displacement), "]"), "\n")
+  cat("Transformation matrix:\n")
+  cat("    ", object$a11, object$a12, object$a13, "\n    ", object$a21, object$a22, object$a23, "\n")
+  cat("Scale:", object$scale, "\n")
+  cat("RMSE:", object$rmse, "\n")
+  cat("RMSE x:", object$rmse_x, "\n")
+  cat("RMSE y:", object$rmse_y, "\n")
   return(invisible(summary_obj))
 }
