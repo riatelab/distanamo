226feb0e6ae7551671529ba4b58f31544d2d7cfe
Matthieu Viry <matthieu.viry@cnrs.fr>
2025-01-23 10:57:44 +0100

    We don't want to do that because cargo checks integrity of files with a checksum

--- dev/vendoring.R
+++ dev/vendoring.R
@@ -1,10 +1,7 @@
 #' The code of this function was copied/pasted from
 #' https://github.com/PRQL/prqlc-r/blob/main/dev/vendoring.R
 #' (licensed under MIT - (c) prqlr authors)
-#' with minor modification to :
-#' - remove the vendor folder after the vendor.tar.xz has been created
-#' - replace cr/crlf line end by lf in savvy-bindgen/src/gen/templates/Makevars.in
-#'   (and any other file if necessary)
+#' with minor modification to remove the vendor folder after the vendor.tar.xz has been created
 vendor_crates <- function(path = ".") {
   src_dir <- rprojroot::find_package_root_file("src", path = path)
 
@@ -25,17 +22,12 @@ vendor_crates <- function(path = ".") {
     )
   )$stdout
 
-  config_toml_content <- stringr::str_replace_all(config_toml_content, "rust/vendor", "vendor")
+  # config_toml_content <- stringr::str_replace_all(config_toml_content, "rust/vendor", "vendor")
 
   brio::write_lines(
     text = config_toml_content,
     path = config_toml_file
   )
-
-  # We need to replace CR/CRLF line endings by LF in
-  # src/rust/vendor/savvy-bindgen/src/gen/templates/Makevars.in in an attempt to fix
-  # https://github.com/riatelab/distanamo/actions/runs/12913965346/job/36012421388#step:6:132
-  find_and_replace_crlf(file.path(src_dir, "rust", "vendor"))
 
   withr::local_dir(file.path(src_dir, vendor_rel_path))
   withr::local_envvar(c(XZ_OPT = "-9"))
@@ -55,26 +47,6 @@ vendor_crates <- function(path = ".") {
   )
 
   unlink(file.path(src_dir, "rust", "vendor"), recursive = TRUE)
-}
-
-replace_crcrlf <- function(file_path) {
-  content <- readLines(file_path, warn = FALSE)
-  content <- stringr::str_replace_all(content, "\r\n|\r", "\n")
-  writeLines(content, file_path, sep = "\n")
-}
-
-find_and_replace_crlf <- function(directory_path) {
-  files_and_dirs <- list.files(directory_path, full.names = TRUE)
-
-  for (item in files_and_dirs) {
-    if (file.info(item)$isdir) {
-      find_and_replace_crlf(item)
-    } else {
-      if (startsWith(basename(item), "Makevars")) {
-        replace_crcrlf(item)
-      }
-    }
-  }
 }
 
 vendor_crates()
